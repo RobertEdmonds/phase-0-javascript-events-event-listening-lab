c813820f453a610d7521f0b89734ee65ea2361ef
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-11-17 18:05:25 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input = document.getElementById('input')
+function addingEventListener(){
+    alert('I was clicked!');
+
+input.addEventListener('click', addingEventListener);
 }
+
